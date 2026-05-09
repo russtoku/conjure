61575585e223d09696bb68344f1561d840061072
Oliver Caldwell <ollie@oli.me.uk>
2026-05-09 10:57:06 +0100

    Add tests for latest tree sitter completions changes

--- fnl/conjure-spec/tree-sitter-completions_spec.fnl
+++ fnl/conjure-spec/tree-sitter-completions_spec.fnl
@@ -1,6 +1,14 @@
-(local {: describe : it} (require :plenary.busted))
+(local {: describe : it : before_each : after_each} (require :plenary.busted))
 (local assert (require :luassert.assert))
+(local res (require :conjure.resources))
+(local ts (require :conjure.tree-sitter))
 (local tsc (require :conjure.tree-sitter-completions))
+
+(var saved-get-resource-contents nil)
+(var saved-parse! nil)
+(var saved-query-parse nil)
+(var saved-language-inspect nil)
+(var saved-get-node nil)
 
 (describe
   "make-prefix-filter"
@@ -20,3 +28,46 @@
           (let [filter (tsc.make-prefix-filter nil)]
             (assert.same [:aaa :word :2342] (filter [:aaa :word :2342])))))))
 
+(describe
+  "get-completions-at-cursor"
+  (fn []
+    (before_each
+      (fn []
+        (set saved-get-resource-contents res.get-resource-contents)
+        (set saved-parse! ts.parse!)
+        (set saved-query-parse vim.treesitter.query.parse)
+        (set saved-language-inspect vim.treesitter.language.inspect)
+        (set saved-get-node vim.treesitter.get_node)))
+
+    (after_each
+      (fn []
+        (tset res :get-resource-contents saved-get-resource-contents)
+        (tset ts :parse! saved-parse!)
+        (tset vim.treesitter.query :parse saved-query-parse)
+        (tset vim.treesitter.language :inspect saved-language-inspect)
+        (tset vim.treesitter :get_node saved-get-node)))
+
+    (it "returns no completions when the tree-sitter language is unavailable"
+        (fn []
+          (tset res :get-resource-contents (fn [_] "(query)"))
+          (tset vim.treesitter.language :inspect
+                (fn [_] (error "language not found")))
+          (tset vim.treesitter.query :parse
+                (fn [_ _] (error "query parse should not be called")))
+
+          (assert.same [] (tsc.get-completions-at-cursor
+                            :missing-test-lang
+                            :missing-test-resource))))
+
+    (it "returns no completions when parsing the current buffer fails"
+        (fn []
+          (tset res :get-resource-contents (fn [_] "(query)"))
+          (tset vim.treesitter.language :inspect (fn [_] {}))
+          (tset vim.treesitter.query :parse (fn [_ _] {}))
+          (tset ts :parse! (fn [] nil))
+          (tset vim.treesitter :get_node
+                (fn [] (error "get_node should not be called")))
+
+          (assert.same [] (tsc.get-completions-at-cursor
+                            :parse-fail-test-lang
+                            :parse-fail-test-resource))))))
--- lua/conjure-spec/tree-sitter-completions_spec.lua
+++ lua/conjure-spec/tree-sitter-completions_spec.lua
@@ -2,8 +2,17 @@
 local _local_1_ = require("plenary.busted")
 local describe = _local_1_.describe
 local it = _local_1_.it
+local before_each = _local_1_.before_each
+local after_each = _local_1_.after_each
 local assert = require("luassert.assert")
+local res = require("conjure.resources")
+local ts = require("conjure.tree-sitter")
 local tsc = require("conjure.tree-sitter-completions")
+local saved_get_resource_contents = nil
+local saved_parse_21 = nil
+local saved_query_parse = nil
+local saved_language_inspect = nil
+local saved_get_node = nil
 local function _2_()
   local function _3_()
     local filter = tsc["make-prefix-filter"]("aa")
@@ -21,4 +30,65 @@ local function _2_()
   end
   return it("filters nothing from aaa word 2342 with prefix nil", _5_)
 end
-return describe("make-prefix-filter", _2_)
+describe("make-prefix-filter", _2_)
+local function _6_()
+  local function _7_()
+    saved_get_resource_contents = res["get-resource-contents"]
+    saved_parse_21 = ts["parse!"]
+    saved_query_parse = vim.treesitter.query.parse
+    saved_language_inspect = vim.treesitter.language.inspect
+    saved_get_node = vim.treesitter.get_node
+    return nil
+  end
+  before_each(_7_)
+  local function _8_()
+    res["get-resource-contents"] = saved_get_resource_contents
+    ts["parse!"] = saved_parse_21
+    vim.treesitter.query["parse"] = saved_query_parse
+    vim.treesitter.language["inspect"] = saved_language_inspect
+    vim.treesitter["get_node"] = saved_get_node
+    return nil
+  end
+  after_each(_8_)
+  local function _9_()
+    local function _10_(_)
+      return "(query)"
+    end
+    res["get-resource-contents"] = _10_
+    local function _11_(_)
+      return error("language not found")
+    end
+    vim.treesitter.language["inspect"] = _11_
+    local function _12_(_, _0)
+      return error("query parse should not be called")
+    end
+    vim.treesitter.query["parse"] = _12_
+    return assert.same({}, tsc["get-completions-at-cursor"]("missing-test-lang", "missing-test-resource"))
+  end
+  it("returns no completions when the tree-sitter language is unavailable", _9_)
+  local function _13_()
+    local function _14_(_)
+      return "(query)"
+    end
+    res["get-resource-contents"] = _14_
+    local function _15_(_)
+      return {}
+    end
+    vim.treesitter.language["inspect"] = _15_
+    local function _16_(_, _0)
+      return {}
+    end
+    vim.treesitter.query["parse"] = _16_
+    local function _17_()
+      return nil
+    end
+    ts["parse!"] = _17_
+    local function _18_()
+      return error("get_node should not be called")
+    end
+    vim.treesitter["get_node"] = _18_
+    return assert.same({}, tsc["get-completions-at-cursor"]("parse-fail-test-lang", "parse-fail-test-resource"))
+  end
+  return it("returns no completions when parsing the current buffer fails", _13_)
+end
+return describe("get-completions-at-cursor", _6_)
